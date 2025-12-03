ced73dfa1c81d7a366c4bdc8839603469eac7f2e
potsamnang <potsamnang68@gmail.com>
2025-12-04 01:17:51 +0700

    Update API URL to use environment variable

--- frontend/src/store/productStore.js
+++ frontend/src/store/productStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
+// src/api.js
+export const API = import.meta.env.VITE_API_URL + "/api/product";
 
-const API = import.meta.env.VITE_API_URL;
 // your backend URL
 
 export const useProductStore = create((set, get) => ({
